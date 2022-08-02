1491e412c94b4e138aecd789daf4c2b444e7f3c8
Bobby Kumar <kashyapashish0002@gmail.com>
2022-08-02 15:43:45 +0530

    Removed No Data fetching issue

---
+++ statics/scripts/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBxKKkdHnrJwmF2xLd0n4xvJLZiigf450Y",
+    authDomain: "javascript-issue-tracker.firebaseapp.com",
+    projectId: "javascript-issue-tracker",
+    storageBucket: "javascript-issue-tracker.appspot.com",
+    messagingSenderId: "737088544776",
+    appId: "1:737088544776:web:824452d708139805bb71bd"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default db;
